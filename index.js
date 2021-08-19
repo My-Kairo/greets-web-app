720fbf00b6aa95edd10176ca09fbfec148c67f98
Thatohatsi Tsoho <marciatsoho@gmail.com>
2021-08-19 13:47:17 +0200

    uncommented

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const session = require('express-session');
 const flash = require('express-flash');
 const Greetings = require('./greetings-factory');
-// const { Pool } = require('pg');
+const { Pool } = require('pg');
 
 const app = express();
 
@@ -27,80 +27,80 @@ app.use(flash());
 app.use(express.static('public'));
 
 
-// let useSSL = false;
-// let local = process.env.LOCAL || false;
-// if (process.env.DATABASE_URL && !local) {
-//     useSSL = true;
-// }
+let useSSL = false;
+let local = process.env.LOCAL || false;
+if (process.env.DATABASE_URL && !local) {
+    useSSL = true;
+}
 
-// const connectionString = process.env.DATABASE_URL || 'postgresql://codex:codex123@localhost:5432/users';
+const connectionString = process.env.DATABASE_URL || 'postgresql://codex:codex123@localhost:5432/users';
 
-// const pool = new Pool({
-//     connectionString: connectionString,
-//     ssl: {
-//         rejectUnauthorized: false
-//     }
+const pool = new Pool({
+    connectionString: connectionString,
+    ssl: {
+        rejectUnauthorized: false
+    }
 
-// });
-// const greeting = Greetings(pool);
-// console.log(pool);
+});
+const greeting = Greetings(pool);
+console.log(pool);
 
-// app.get('/', async function (req, res) {
-//     res.render('index', {
-//         greetedTimes: await greeting.poolTable()
-//     })
-// })
+app.get('/', async function (req, res) {
+    res.render('index', {
+        greetedTimes: await greeting.poolTable()
+    })
+})
 
-// app.post('/greetings-factory', async function (req, res, next) {
+app.post('/greetings-factory', async function (req, res, next) {
 
-//     try {
-//         let hello = req.body.language;
-//         let string = req.body.textBoxBttn;
-//         if (hello == undefined && string == "") {
-//             req.flash('info', 'Please enter the name!')
-//         }
-//         else if (hello == undefined) {
-//             req.flash('info', 'Please select a language!');
-//         } else if (string == "") {
-//             req.flash('info', 'Please enter a valid name!')
-//         } else {
-//             greeting.greetMessage(hello, string);
-//             await greeting.poolName(string);
-//         };
-//         res.render('index', { 
-//             greetMe: greeting.getGreet(), 
-//             greetedTimes: await greeting.poolTable() 
-//         });
-//     } catch (error) {
-//         next(error);
+    try {
+        let hello = req.body.language;
+        let string = req.body.textBoxBttn;
+        if (hello == undefined && string == "") {
+            req.flash('info', 'Please enter the name!')
+        }
+        else if (hello == undefined) {
+            req.flash('info', 'Please select a language!');
+        } else if (string == "") {
+            req.flash('info', 'Please enter a valid name!')
+        } else {
+            greeting.greetMessage(hello, string);
+            await greeting.poolName(string);
+        };
+        res.render('index', { 
+            greetMe: greeting.getGreet(), 
+            greetedTimes: await greeting.poolTable() 
+        });
+    } catch (error) {
+        next(error);
 
-//     }
-// })
+    }
+})
 
-// app.get("/greeted", async (req, res) => {
-//     res.render("greeted", { model: await greeting.greeted() });
-// });
+app.get("/greeted", async (req, res) => {
+    res.render("greeted", { model: await greeting.greeted() });
+});
 
-// app.get('/counter/:userName', async function(req, res){
-//     var name = req.params.userName;
-//     res.render("counter", { name: await greeting.getUserName(name) });
-// })
+app.get('/counter/:userName', async function(req, res){
+    var name = req.params.userName;
+    res.render("counter", { name: await greeting.getUserName(name) });
+})
 
-// app.post('/reset',async function (req, res) {
-//     req.flash('info', 'Database deleted successfully');
-//     await greeting.emptyDB();
-//     res.redirect('/');
-// })
+app.post('/reset',async function (req, res) {
+    req.flash('info', 'Database deleted successfully');
+    await greeting.emptyDB();
+    res.redirect('/');
+})
 
-// app.post('/clear', async function (req, res) {
-//     req.flash('info', 'Database deleted successfully');
-//     await greeting.emptyDB();
-//     res.render('greeted');
-// })
+app.post('/clear', async function (req, res) {
+    req.flash('info', 'Database deleted successfully');
+    await greeting.emptyDB();
+    res.render('greeted');
+})
 
-// app.post('/home', function (req, res) {
-//     res.redirect('/')
-// })
+app.post('/home', function (req, res) {
+    res.redirect('/')
+})
 
 let PORT = process.env.PORT || 2018;
 app.listen(PORT, function(){
