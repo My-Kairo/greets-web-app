1272c04a45983eea94d6d0f46b6bf6b74152e0d0
Thatohatsi Tsoho <marciatsoho@gmail.com>
2021-09-30 15:41:44 +0200

    tests

--- test/greetings-test.js
+++ test/greetings-test.js
@@ -4,8 +4,7 @@ const pg = require("pg");
 const Pool = pg.Pool;
 
 const connectionString =
-  process.env.DATABASE_URL ||
-  "postgresql://codex:pg123@localhost:5432/greeting";
+  process.env.DATABASE_URL || "postgresql://codex:pg123@localhost:5432/greeting";
 
   const pool = new Pool({
     connectionString,
